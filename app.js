e04a4210b9c68d8a9b420ca1c398fb6d5400a3a5
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-05-24 16:40:03 -0700

    primitive formatting

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const weatherDisplay = document.getElementById("weatherbox");
-const weatherText = document.createElement("div");
+
+const cityName = document.createElement("div");
+cityName.classList.add("cityName");
+
+const temperature1 = document.createElement("div");
+temperature1.classList.add("temperature");
+
+const temperature2 = document.createElement("div");
+temperature2.classList.add("temperature");
+
+const weatherImage = document.createElement("img")
 
 const searchBtn = document.getElementById("search");
 
@@ -13,16 +23,34 @@ searchBtn.addEventListener("click", event => {
 
     event.preventDefault();
     const searchedCity = document.getElementById("searchText").value;
-    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${apiKey}`
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${apiKey}&units=metric`
 
     async function getWeather() {
         const response = await fetch(apiURL);
         const weatherData = await response.json();
 
-        console.log(weatherData)
+        console.log(weatherData);
+
+        const city = weatherData.name;
+        cityName.innerText = city;
+        weatherDisplay.appendChild(cityName);
+
+        //weather icon:
+        const weatherId = weatherData.weather[0].icon;
+
+        weatherImage.src = `http://openweathermap.org/img/wn/${weatherId}@2x.png`;
+        weatherDisplay.appendChild(weatherImage);
+
+        //default measurements are in metric (units=metric) via api call:
+        const tempCelsius = parseInt(weatherData.main.temp.toFixed(2));
+        const tempFahrenheit = parseInt(((tempCelsius * 1.8) + 32).toFixed(2));
 
 
+        temperature1.innerText = `${tempCelsius} °c`;
+        weatherDisplay.appendChild(temperature1);
 
+        temperature2.innerText = `${tempFahrenheit} °f`;
+        weatherDisplay.appendChild(temperature2);
 
     }
     getWeather();
